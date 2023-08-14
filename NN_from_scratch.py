9290d521697669aee782a22af25607bd4b15b97b
William yan <1337842463@qq.com>
2023-08-14 15:17:41 +0800

    __init__

--- NN_from_scratch.py
+++ NN_from_scratch.py
@@ -78,7 +78,46 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
 
 ## training loop ##
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train() # 这个的作用想当于 with torch.no_grad()=False 也就是说要更新参数
+    for batch, (X,y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred,y)
 
+        # Backpropagation
+        loss.backward()
+        optimizer.step() # 这一步是更新参数
+        optimizer.zero_grad() # 梯度清零,假如没有这一步，这次计算的梯度会和上次的梯度累加，这样的话会一直记着上次的梯度，损耗memory
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch*len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+## test loop ##
+def test_loop(dataloader, model, loss_fn):
+    model.eval() # 等于with torch.no_grad()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X,y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred,y).item()
+            correct += (pred.argmax(1)==y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+loss_fn = loss_fn
+optimizer = optimizer
+epochs = epochs
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
+
+### save and load the model ###
 
 
 
