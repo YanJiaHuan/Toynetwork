7fe67e3df6a8229c3151e6030db4656bbe4e47f8
William yan <1337842463@qq.com>
2023-08-15 16:41:35 +0800

    __init__

--- seq2seq_lstm.py
+++ seq2seq_lstm.py
@@ -11,11 +11,13 @@ from torch.utils.tensorboard import SummaryWriter
 
 ### load data ###
 import en_core_web_sm
-import zh_core_web_sm
-spacy_ger = zh_core_web_sm.load()
+import de_core_news_sm
+spacy_ger = de_core_news_sm.load()
 spacy_eng = en_core_web_sm.load()
 
 ### help functions ###
+sos_tok = '<sos>'
+eos_tok = '<eos>'
 def tokenize_ger(text):
     return [tok.text for tok in spacy_ger.tokenizer(text)]
 
@@ -26,3 +28,31 @@ text = 'I like apples'
 doc = spacy_eng(text)
 for token in doc:
     print(token.text, token.pos_, token.dep_)
+
+train_data, valid_data, test_data = Multi30k(root='.data',split=('train', 'valid', 'test'), language_pair=('de', 'en'))
+
+
+class TextDatasets(torch.utils.data.Dataset):
+    def __init__(self, raw_data):
+        self.datasets = list(raw_data)
+
+    def __len__(self):
+        return len(self.datasets)
+
+    def __getitem__(self, idx):
+        src, trg = self.datasets[idx]
+        src = [sos_tok] + tokenize_ger(src) + [eos_tok]
+        trg = [sos_tok] + tokenize_eng(trg) + [eos_tok]
+        return src, trg
+
+
+train_datasets = TextDatasets(train_data)
+valid_datasets = TextDatasets(valid_data)
+test_datasets = TextDatasets(test_data)
+
+print("Train size:", len(train_datasets))
+print("Valid size:", len(valid_datasets))
+print("Test size:", len(test_datasets))
+
+# Define the fields associated with the sequences.
+
