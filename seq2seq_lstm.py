69109efd7893511c9ceaac0f8b84b52774d2545b
William yan <1337842463@qq.com>
2023-08-16 11:12:39 +0800

    __init__

--- seq2seq_lstm.py
+++ seq2seq_lstm.py
@@ -15,6 +15,7 @@ import de_core_news_sm
 spacy_ger = de_core_news_sm.load()
 spacy_eng = en_core_web_sm.load()
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 ### help functions ###
 sos_tok = '<sos>'
 eos_tok = '<eos>'
@@ -56,14 +57,92 @@ print("Test size:", len(test_datasets))
 
 ### network ###
 class Encoder(nn.Module):
-    pass
-
+    def __init__(self,input_size,embedding_size,hidden_size,num_layers,p):
+        super(Encoder,self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = nn.Dropout(p)
+        self.embedding = nn.Embedding(input_size,embedding_size)
+        self.rnn = nn.LSTM(embedding_size,hidden_size,num_layers,dropout=p)
+    def forward(self,x):
+        # x shape: (seq_length, N) where N is batch size
+        embedding = self.dropout(self.embedding(x))
+        # embedding shape: (seq_length, N, embedding_size)
+        outputs, (hidden, cell) = self.rnn(embedding)
+        return hidden, cell
 class Decoder(nn.Module):
-    pass
+    def __init__(self,input_size, embedding_size, hidden_size, output_size, num_layers, p):
+        super(Decoder,self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = nn.Dropout(p)
+        self.embedding = nn.Embedding(input_size,embedding_size)
+        self.rnn = nn.LSTM(embedding_size,hidden_size,num_layers,dropout=p)
+        self.fc = nn.Linear(hidden_size,output_size)
+    def forward(self,x,hidden,cell):
+# shape of x: (N) but we want (1,N)
+        x = x.unsqueeze(0)
+        embedding = self.dropout(self.embedding(x))
+        # embedding shape: (1,N,embedding_size)
+        outputs, (hidden, cell) = self.rnn(embedding, (hidden, cell))
+        # shape of outputs: (1,N,hidden_size)
+        predictions = self.fc(outputs)
+        # shape of predictions: (1,N,length_of_vocab)
+        predictions = predictions.squeeze(0)
+        return predictions, hidden, cell
 
+class seq2seq(nn.Module):
+    def __init__(self,encoder,decoder):
+        super(seq2seq,self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+    def forward(self,source,target,teacher_force_ratio=0.5):
+        batch_size = source.shape[1]
+        target_len = target.shape[0]
+        target_vocab_size = len(german.vocab)
+        outputs = torch.zeros(target_len,batch_size,target_vocab_size).to(device)
+        hidden, cell = self.encoder(source)
+        x = target[0] # get the first token
+        for t in range(1,target_len):
+            output, hidden, cell = self.decoder(x,hidden,cell)
+            outputs[t] = output
+            best_guess = output.argmax(1)
+            x = target[t] if random.random() < teacher_force_ratio else best_guess
+        return outputs
 
+### training ###
+## hyperparameters ##
+num_epochs = 20
+learning_rate = 1e-3
+batch_size = 64
 
+## model parameters ##
+load_model = False
+device = device
+input_size_encoder = len(german.vocab)
+input_size_decoder = len(english.vocab)
+output_size = len(english.vocab)
+encoder_embedding_size = 300
+decoder_embedding_size = 300
+hidden_size = 1024
+num_layers = 2
+enc_dropout = 0.5
+dec_dropout = 0.5
 
+## tensorboard ##
+writer = SummaryWriter(f'runs/loss_plot')
+step = 0
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size=batch_size,
+    sort_within_batch=True,
+    sort_key=lambda x: len(x.src),
+    device=device)
+encoder_net = Encoder(input_size_encoder,encoder_embedding_size,hidden_size,num_layers,enc_dropout).to(device)
+decoder_net = Decoder(input_size_decoder,decoder_embedding_size,hidden_size,output_size,num_layers,dec_dropout).to(device)
+
+model = seq2seq(encoder_net,decoder_net).to(device)
 
 
 
