587986dfc4bd84f4e703ac820e410fbf8b4fbfdb
William yan <1337842463@qq.com>
2023-08-24 13:01:33 +0800

    __init__

--- MachineTranslation_Transformer.py
+++ MachineTranslation_Transformer.py
@@ -16,8 +16,8 @@ dataset_id = 'bentrevett/multi30k'
 en_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 ge_tokenizer = AutoTokenizer.from_pretrained('bert-base-german-cased')
 
-print(en_tokenizer(['Hello, my dog is cute. [SEP] I hate dogs']))
-print(ge_tokenizer('Hallo, mein Hund ist süß'))
+# print(en_tokenizer(['Hello, my dog is cute. [SEP] I hate dogs']))
+# print(ge_tokenizer('Hallo, mein Hund ist süß'))
 # 这里的attention mask 是告诉模型哪些是padding的，哪些是真实的token，padding的就是0，也代表不需要进行attention计算
 
 ## process data ##
@@ -30,4 +30,51 @@ def process_fn(examples):
 data = datasets.load_dataset(dataset_id, 'de-en')
 data = data.map(process_fn, batched=True)
 train_data, val_data, test_data = data['train'], data['validation'], data['test']
+# print(train_data[0])
+
+class Transformer(nn.Module):
+    def __init__(
+            self,
+            src_vocab_size,
+            trg_vocab_size,
+            src_pad_idx,
+            trg_pad_idx,
+            device,
+            embed_size,
+            num_encoder_layers,
+            num_decoder_layers,
+            forward_expansion,
+            num_heads,
+            dropout,
+            max_length
+    ):
+        super(Transformer,self).__init__()
+        self.src_word_embedding = nn.Embedding(src_vocab_size,embed_size)
+        self.src_position_embedding = nn.Embedding(max_length,embed_size)
+        self.trg_word_embedding = nn.Embedding(trg_vocab_size,embed_size)
+        self.trg_position_embedding = nn.Embedding(max_length,embed_size)
+        self.device = device
+        self.transformer = nn.Transformer(
+            d_model=embed_size,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dim_feedforward=forward_expansion*embed_size,
+            dropout=dropout
+        )
+        self.fc_out = nn.Linear(embed_size,trg_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.max_length = max_length
+
+    def forward(self,src,trg):
+        src_seq_length, N = src.shape
+        trg_seq_length, N = trg.
+
+
+
+
+
+
 # CUDA_VISIBLE_DEVICES=7 python3 MachineTranslation_Transformer.py
--- README.md
+++ README.md
@@ -49,3 +49,11 @@ In transformer, embed_size = head_size * num_heads, and the matrix of query, key
 With the **increase** of num_heads, the computation loop **increases**, which made the total computation cost **increases**. But the
 good part is **more** iteration of computation can be introduced, which means **more** aspects of attention is considered.  
 From my perspective, **increase** num_heads --> **increase** computation cost --> **decrease** attention block --> make model more focus on details.
+
+#### For example:
+Bert-base has 12 layers, 12 heads, 768 embed_size.  
+Bert-large has 24 layers, 16 heads, 1024 embed_size.  
+Bert-large: 340M parameters, Bert-base: 110M parameters.  
+x = Bert-base.para, y = Bert-large.para
+y = (24/12) * (1024/768) * x = 2.67x   
+110M * 2.67 = 293.7M, which is close to 340M, normally there will be some other parameters inside.
